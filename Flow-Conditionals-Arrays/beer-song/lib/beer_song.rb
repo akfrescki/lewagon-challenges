3689c656b23c59ee43c6b947889382370e6ca532
akfrescki <karolinefrescki@gmail.com>
2026-02-03 19:15:31 +0100

    completed interface and beer_song files

--- Flow-Conditionals-Arrays/beer-song/lib/beer_song.rb
+++ Flow-Conditionals-Arrays/beer-song/lib/beer_song.rb
@@ -1,3 +1,17 @@
-def beer_song(beer_start_count)
 
+def beer_song(beer_start_count)
+    lines = []
+    
+    beer_start_count.downto(1) do |count|
+        next_count = count - 1
+
+        line_1 = "#{count} bottles of beer on the wall, #{count} bottles of beer!"
+        next_phrase = next_count == 0 ? "no more bottles" : "#{next_count} bottles"
+        line_2 = "Take one down, pass it around, #{next_phrase} of beer on the wall!"
+        lines << line_1
+        lines << line_2
+    end
+
+    lines.join("\n")
+    
 end
--- Flow-Conditionals-Arrays/beer-song/lib/interface.rb
+++ Flow-Conditionals-Arrays/beer-song/lib/interface.rb
@@ -1,4 +1,15 @@
-require relative 'beer_song'
+require_relative 'beer_song'
+# always require the file with the method you want to call in the interface.rb file
 
-beer_start_count = nil
-beer_song(beer_start_count)
+# get the argument from the command line and convert it to an integer
+beer_start_count = ARGV[0].to_i
+# call method always after getting the argument from the command line
+# convert the variable to a song one to store the replacment of the bottle
+song = beer_song(beer_start_count)
+
+# song will have all the lines of the beer song
+# replace the "1 bottles of beer" with "1 bottle of beer"
+# thats why use gsub method
+song = song.gsub("1 bottles of beer", "1 bottle of beer")
+
+puts song
